fcb8d36ed54c8802989b36f294c244f17dfd764e
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-02-28 13:06:01 +0100

    Update SoapLibraryV2.lua

--- SoapLibraryV2.lua
+++ SoapLibraryV2.lua
@@ -46,8 +46,7 @@ function SoapLibrary:Init(options)
 		--[[ MAIN FRAME ]]--
 
 		-- StarterGui.SoapLibrary
-		GUI["1"] = Instance.new("ScreenGui");
-		if runService:IsStudio() then GUI["1"].Parent = coreGui else GUI["1"].Parent = lp.PlayerGui end
+		GUI["1"] = Instance.new("ScreenGui", coreGui);
 		GUI["1"]["Name"] = [[SoapLibrary]];
 		GUI["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 
@@ -387,6 +386,40 @@ function SoapLibrary:Init(options)
 						GUI["2"].Visible = true
 					end
 					end
+			end
+		end)
+		
+		local frame = GUI["2"]
+		local dragToggle = nil
+		local dragSpeed = 1
+		local dragStart = nil
+		local startPos = nil
+
+		local function updateInput(input)
+			local delta = input.Position - dragStart
+			local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+				startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+			game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+		end
+
+		frame.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+				dragToggle = true
+				dragStart = input.Position
+				startPos = frame.Position
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+
+		uis.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				if dragToggle then
+					updateInput(input)
+				end
 			end
 		end)
 		
